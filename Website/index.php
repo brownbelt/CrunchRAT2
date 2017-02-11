40251a7529628864eb6f4b59b7594a5c7d095db1
Hunter Hardman <t3ntman@gmail.com>
2017-02-11 16:19:24 -0700

    Hopefully fixed weird red highlight over ampersand

--- Website/index.php
+++ Website/index.php
@@ -11,7 +11,7 @@
         <title>CrunchRAT</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <!-- CSS -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
         <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
         <link href="plugins/node-waves/waves.css" rel="stylesheet" />
