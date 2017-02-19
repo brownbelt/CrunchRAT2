2ce68e0f433797ed9afb00b7244ff80750b3308e
Hunter Hardman <t3ntman@gmail.com>
2017-02-19 09:55:24 -0700

    Changed database table from "hosts" to "implants"

--- Setup/database_setup.sql
+++ Setup/database_setup.sql
@@ -1,5 +1,5 @@
-CREATE TABLE hosts (`hostname` TEXT(65535), `current_user` TEXT(65535), `process_id` TEXT(65535), `os` TEXT(65535), `last_seen` TEXT(65535));
+CREATE TABLE implants (`hostname` TEXT(65535), `current_user` TEXT(65535), `process_id` TEXT(65535), `os` TEXT(65535), `last_seen` TEXT(65535));
 
-INSERT INTO hosts (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "1781", "Mac OS X 10.8", "2017-02-15 01:32:17");
-INSERT INTO hosts (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "2053", "Mac OS X 10.8", "2017-02-15 01:33:10");
-INSERT INTO hosts (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("t3ntman's MacBook Pro", "t3ntman", "6429", "Mac OS X 10.9", "2017-02-15 10:26:49");
+INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "1781", "Mac OS X 10.8", "2017-02-15 01:32:17");
+INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "2053", "Mac OS X 10.8", "2017-02-15 01:33:10");
+INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("t3ntman's MacBook Pro", "t3ntman", "6429", "Mac OS X 10.9", "2017-02-15 10:26:49");
