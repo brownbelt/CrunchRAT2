7f959f54e496e9f06b44712cbe49e995c2fe7526
Hunter Hardman <t3ntman@gmail.com>
2017-02-22 15:04:36 -0700

    Apparently you don't need to specify size limits with TEXT

--- Setup/database_setup.sql
+++ Setup/database_setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE implants (`hostname` TEXT(65535), `current_user` TEXT(65535), `process_id` TEXT(65535), `os` TEXT(65535), `last_seen` TEXT(65535));
+CREATE TABLE implants (`hostname` TEXT, `current_user` TEXT, `process_id` TEXT, `os` TEXT, `last_seen` TEXT);
 
 INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "1781", "Mac OS X 10.8", "2017-02-15 01:32:17");
 INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "2053", "Mac OS X 10.8", "2017-02-15 01:33:10");
