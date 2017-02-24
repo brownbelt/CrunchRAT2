466041e875de43177fa31e8b36686c8c4a42d280
Hunter Hardman <t3ntman@gmail.com>
2017-02-24 15:00:02 -0700

    Cleaned up hashing function

--- Website/hashing.php
+++ Website/hashing.php
@@ -1,16 +1,11 @@
 <?php
-    # This function generates a salted hash using bcrypt
-    # Modified from original source: https://www.sitepoint.com/password-hashing-in-php
-    function generate_hash($password) {
-        # If bcrypt is supported on the system
+    function generate_salted_hash($password) {
+        # Purpose: This function takes a password and generates a salted hash using bcrypt
+        # Returns: Array containing hashed password and salt (22 randomly-generated characters)
+        # Modified from original source: https://www.sitepoint.com/password-hashing-in-php
         if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
-            # Generates 22-character random salt
             $salt = substr(md5(uniqid(rand(), true)), 0, 22);
-
-            # Generates hashed password (with salt)
             $hash = crypt($password, $salt);
-
-            # Returns hashed password and salt in an array
             return array($hash, $salt);
         }
     }
