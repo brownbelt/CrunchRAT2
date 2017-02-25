f72cd1726ea60e9715b8bb95282cf44582d44a45
root <t3ntman@gmail.com>
2017-02-25 15:05:11 -0700

    Added 403 file

---
+++ Website/403.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>CrunchRAT</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap-theme.min.css">
+        <style>
+            body {
+                background-image: url("images/Ed.jpg");
+                background-position: left top;
+                background-repeat: no-repeat;
+                background-color: black;
+            }
+        </style>
+    </head>
+
+    <body class="text-center"><br>
+        <div class='alert alert-danger'>
+            <strong>You are not authorized for the requested resource.</strong>
+        </div>
+    </body>
+
+    <!-- JavaScript -->
+    <script src="plugins/bootstrap/js/bootstrap.min.js"</script>
+</html>
