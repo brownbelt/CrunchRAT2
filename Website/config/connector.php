4fe8f9fdb505a37ea3f921a4719f7cc31cfc147f
Hunter Hardman <t3ntman@gmail.com>
2017-02-24 17:45:33 -0700

    Added in new database connection statement

--- Website/config/connector.php
+++ Website/config/connector.php
@@ -1,5 +1,6 @@
 <?php
-	$database_user = "root";
-	$database_pass = "root";
-	$database_name = "RAT";
+    $database_user = "root";
+    $database_pass = "root";
+    $database_name = "RAT";
+    $database_connection = new PDO("mysql:host=localhost;dbname=$database_name", $database_user, $database_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
