d542c573f317cfe206d4cbc695ff125fbec6ea11
root <t3ntman@gmail.com>
2017-03-04 14:13:53 -0700

    Added in 404 page

---
+++ Website/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>CrunchRAT</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap-theme.min.css">
+        <style>
+            body {
+                background-image: url("images/Ed.jpg");
+                background-position: left top;
+                background-repeat: no-repeat;
+                background-color: black;
+            }
+        </style>
+    </head>
+
+    <body class="text-center"><br>
+        <div class='alert alert-danger'>
+            <strong>Something went wrong.</strong>
+        </div>
+        <h5>Click <a class="text-info" href="index.php">here</a> to return home</h5>
+    </body>
+
+    <!-- JavaScript -->
+    <script src="plugins/bootstrap/js/bootstrap.min.js"</script>
+</html>
