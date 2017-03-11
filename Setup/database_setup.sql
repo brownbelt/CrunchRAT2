419b05ac43ef908feb75f630d47a62ce85256a6a
Hunter Hardman <t3ntman@gmail.com>
2017-03-11 16:45:16 -0700

    Removed unnecessary columns in the "listeners" table

--- Setup/database_setup.sql
+++ Setup/database_setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE implants (`hostname` TEXT, `current_user` TEXT, `process_id` TEXT, `os` TEXT, `last_seen` TEXT);
 CREATE TABLE users (`username` TEXT, `hashed_password` TEXT, `salt` TEXT);
-CREATE TABLE listeners (`external_address` TEXT, `protocol` TEXT, `port` TEXT, `beacon_uri` TEXT, `update_uri` TEXT, `user_agent` TEXT, `executed_out_of` TEXT, `crt_file` TEXT, `key_file` TEXT);
+CREATE TABLE listeners (`external_address` TEXT, `protocol` TEXT, `port` TEXT, `beacon_uri` TEXT, `update_uri` TEXT, `user_agent` TEXT, `executed_out_of` TEXT);
 CREATE TABLE tasks (`hostname` TEXT, `process_id` TEXT, `task_action` TEXT, `task_secondary` TEXT, `unique_id` TEXT);
 
 INSERT INTO implants (`hostname`, `current_user`, `process_id`, `os`, `last_seen`) VALUES ("Hunter's iMac", "user", "1781", "Mac OS X 10.8", "2017-02-15 01:32:17");
