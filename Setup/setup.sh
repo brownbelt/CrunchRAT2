9a1211cd21ecc0837cb8c2a5974209fd9619cf34
Hunter Hardman <t3ntman@gmail.com>
2017-03-17 10:07:38 -0600

    Added in Ubuntu 16.04 dependencies

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -5,6 +5,14 @@ DEFAULT="\e[39m\e[1m"
 RED="\e[31m\e[1m"
 GREEN="\e[32m\e[1m"
 
+# If script did not run with root or sudo privileges
+# Exits the script
+if [ "$(id -u)" != "0" ];
+then
+    echo -e "${RED}[-] You must run this script with root or sudo privileges.${DEFAULT}\n"
+    exit 1
+fi
+
 # Gets operating system
 OS_NAME=`cat /etc/os-release | grep -i "PRETTY_NAME" | awk -F= '{print $2}' | sed -e 's/"//g'`
 
@@ -12,15 +20,33 @@ OS_NAME=`cat /etc/os-release | grep -i "PRETTY_NAME" | awk -F= '{print $2}' | se
 if [[ $OS_NAME == *"Ubuntu 16.04"* ]]
 then
     echo -e "${GREEN}[+] Ubuntu 16.04 detected ${DEFAULT}\n"
-    VERSION="16.04"
+
+    # Updates repositories
+    echo -e "${GREEN}[+] Updating ${DEFAULT}\n"
+    sudo apt-get update
+
+    # Installs all Ubuntu 16.04 dependencies
+    echo -e "\n${GREEN}[+] Installing dependencies ${DEFAULT}\n"
+    sudo apt-get install -y apache2
+    sudo apt-get install -y mysql-server
+    sudo apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
+
 # Else if Ubuntu 14.04
 elif [[ $OS_NAME == *"Ubuntu 14.04"* ]]
 then
     echo -e "${GREEN}[+] Ubuntu 14.04 detected ${DEFAULT}\n"
-    VERSION="14.04"
+    
+    # Updates repositories
+    echo -e "${GREEN}[+] Updating ${DEFAULT}\n"
+    sudo apt-get update
+
+    # TO DO: Install all Ubuntu 14.04 dependencies here
+
 # Else invalid operating system
 # Exits program
 else
     echo -e "${RED}[-] Invalid operating system ${DEFAULT}\n"
     exit 1
 fi
+
+# TO DO: MySQL database and table setup here
