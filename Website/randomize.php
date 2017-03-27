379eddef136b3203d94dafae49c39cf39b86382f
Hunter Hardman <t3ntman@gmail.com>
2017-03-27 15:22:40 -0600

    Created function to generate a random 10-character string

---
+++ Website/randomize.php
@@ -0,0 +1,18 @@
+<?php
+    function generate_random_string() {
+        # Description: Generates a random 10-character string
+        # Source: http://www.xeweb.net/2011/02/11/generate-a-random-string-a-z-0-9-in-php
+
+        $length = 10;
+        $generated_string = "";
+        $allowed_characters = array_merge(range("A","Z"), range("a","z"), range("0","9"));
+        $max = count($allowed_characters) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $random = mt_rand(0, $max);
+            $generated_string .= $allowed_characters[$random];
+        }
+
+        return $generated_string;
+    }
+?>
