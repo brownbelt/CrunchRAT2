dbcf154676bb3b6f26ee6eaf11e9a34fd4ae625b
root <t3ntman@gmail.com>
2017-03-31 18:43:38 -0600

    Changed randomize.php to generate random 2048 character string

--- Website/randomize.php
+++ Website/randomize.php
@@ -1,9 +1,9 @@
 <?php
     function generate_random_string() {
-        # Description: Generates a random 10-character string
+        # Description: Generates a random 2048 character string
         # Source: http://www.xeweb.net/2011/02/11/generate-a-random-string-a-z-0-9-in-php
 
-        $length = 10;
+        $length = 2048;
         $generated_string = "";
         $allowed_characters = array_merge(range("A","Z"), range("a","z"), range("0","9"));
         $max = count($allowed_characters) - 1;
