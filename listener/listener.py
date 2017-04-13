9729bf9e28679e08f3f12efebbf9cf9da8ec436f
Hunter Hardman <t3ntman@gmail.com>
2017-04-12 18:22:30 -0600

    Added ArgChecks class

--- listener/listener.py
+++ listener/listener.py
@@ -5,9 +5,7 @@ import socket
 import sys
 from colorama import Fore, Style
 from flask import Flask
-
-# DEBUGGING
-from core.server import Server
+from core.argchecks import ArgChecks
 
 app = Flask(__name__)
 
@@ -22,67 +20,13 @@ if __name__ == "__main__":
     parser.add_argument("protocol", nargs="?", type=str, help="listener protocol [http][https]")
     parser.add_argument("external_address", nargs="?", type=str, help="listener external address")
     parser.add_argument("port", nargs="?", type=int, help="listener port")
-    parser.add_argument("profile", nargs="?", type=argparse.FileType("r"), help="json profile")
+    #parser.add_argument("profile", nargs="?", type=argparse.FileType("r"), help="json profile")
+    parser.add_argument("profile", nargs="?", type=str, help="json profile")
     args = parser.parse_args()
 
-    # saves provided arguments
-    protocol = args.protocol
-    external_address = args.external_address
-    port = args.port
-    profile = args.profile
-
-    # if protocol is not "http" or "https"
-    # prints error and exits the program
-    if protocol != "http" and protocol != "https":
-        print(Style.BRIGHT + Fore.RED + '[!] Invalid protocol supplied. Please enter "http" or "https".' + Style.RESET_ALL)
-        sys.exit(0)
-
-    # TO DO: add in check for "https" so we can add in the flask ssl context
-
-    # checks if supplied address is valid
-    try:
-        ipaddress.ip_address(external_address)
-
-    # exception raised means invalid external address was provided
-    # prints error and exits the program
-    except:
-        print(Style.BRIGHT + Fore.RED + "[!] Invalid external address supplied." + Style.RESET_ALL)
-        sys.exit(0)
-
-    # checks if port is available
-    # prints error message and exits if the port is in use
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    try:
-        s.connect(("localhost", port))
-        print(Style.BRIGHT + Fore.RED + "[!] Port already in use. Please choose another port." + Style.RESET_ALL)
-        sys.exit(0)
-
-    except:
-        pass
-
-    # tries to load and parse the json profile
-    # exception raised means an invalid json profile was provided
-    try:
-        with open("profiles/pandora.json") as data_file:
-            profile_json = json.load(profile)
-
-            # DEBUGGING - NOT SOLD ON THE VARIABLE NAME ABOVE
-            print(profile_json)
-
-    # prints error and exits the program
-    except:
-        print(Style.BRIGHT + Fore.RED + "[!] Error parsing the provided JSON profile." + Style.RESET_ALL)
-        sys.exit(0)
-
-    # all arguments have been validated at this point
-
-    # DEBUGGING
-    s = Server()
-    s.beacon()
-    s.update()
-
-    # TO DO: INSERT entry into "listeners" table
-
-    # TO DO: start flask listener
-    #app.run("0.0.0.0", port)
+    # performs argument checks
+    c = ArgChecks()
+    c.protocol_check(args.protocol)
+    c.external_address_check(args.external_address)
+    c.port_check(args.port)
+    c.profile_check(args.profile)
