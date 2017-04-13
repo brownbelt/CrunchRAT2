e1a0903ae5dcc85834b1ef2312207527ba93e3b2
Hunter Hardman <t3ntman@gmail.com>
2017-04-12 21:04:08 -0600

    Removed "json" from "json_profile" column

--- Setup/database_setup.sql
+++ Setup/database_setup.sql
@@ -1,4 +1,4 @@
 CREATE TABLE implants (`hostname` TEXT, `current_user` TEXT, `process_id` TEXT, `operating_system` TEXT, `last_seen` TEXT, `encryption_key` TEXT);
 CREATE TABLE users (`username` TEXT, `hashed_password` TEXT, `salt` TEXT);
-CREATE TABLE listeners (`id` INT(16) AUTO_INCREMENT, `external_address` TEXT, `protocol` TEXT, `port` TEXT, `json_profile` TEXT, PRIMARY KEY (`id`));
+CREATE TABLE listeners (`id` INT(16) AUTO_INCREMENT, `external_address` TEXT, `protocol` TEXT, `port` TEXT, `profile` TEXT, PRIMARY KEY (`id`));
 CREATE TABLE tasks (`hostname` TEXT, `process_id` TEXT, `task_action` TEXT, `task_secondary` TEXT, `unique_id` TEXT);
