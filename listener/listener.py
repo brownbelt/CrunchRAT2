f12e7ded2345c789937deacbe636496c054425f8
Hunter Hardman <t3ntman@gmail.com>
2017-04-12 18:40:15 -0600

    Added sudo/root privileges check

--- listener/listener.py
+++ listener/listener.py
@@ -1,26 +1,24 @@
+#!/usr/bin/python3
+
 import argparse
-import ipaddress
-import json
-import socket
+import os
 import sys
 from colorama import Fore, Style
-from flask import Flask
 from core.argchecks import ArgChecks
-
-app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    # TO DO: add in checks for python 2.x
-
-    # TO DO: add in sudo checks
+    # if not ran with sudo or root privileges
+    # exits the program
+    if os.geteuid() != 0:
+        print(Style.BRIGHT + Fore.RED + "[!] Please re-run with sudo or root privileges." + Style.RESET_ALL)
+        sys.exit()
 
     # parses arguments
     parser = argparse.ArgumentParser(prog="listener.py", description="CrunchRAT2 Listener - Written by Hunter Hardman @t3ntman")
     parser.add_argument("protocol", nargs="?", type=str, help="listener protocol [http][https]")
     parser.add_argument("external_address", nargs="?", type=str, help="listener external address")
     parser.add_argument("port", nargs="?", type=int, help="listener port")
-    #parser.add_argument("profile", nargs="?", type=argparse.FileType("r"), help="json profile")
     parser.add_argument("profile", nargs="?", type=str, help="json profile")
     args = parser.parse_args()
 
