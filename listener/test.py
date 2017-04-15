e0998eecfe11f1afcb9f8708aa2bb1ccf74080d3
Hunter Hardman <t3ntman@gmail.com>
2017-04-15 10:14:34 -0600

    Added in debugging file

---
+++ listener/test.py
@@ -0,0 +1,38 @@
+import json
+from flask import Flask
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+# temporarily globally-accessible and hard-coded
+with open("pandora_new.json") as data_file:
+    data = json.load(data_file)
+
+
+def beacon():
+    resp = make_response()
+
+    # loops through JSON elements and adds in malleable HTTP headers
+    for name in data["beacon_response_headers"]:
+        resp.headers[name] = data["beacon_response_headers"][name]
+
+    resp.data = "beacon response"
+    return resp
+
+
+def update():
+    resp = make_response()
+
+    # loops through JSON elements and adds in malleable HTTP headers
+    for name in data["update_response_headers"]:
+        resp.headers[name] = data["update_response_headers"][name]
+
+    resp.data = "update response"
+    return resp
+
+
+if __name__ == "__main__":
+    app.add_url_rule(data["implant"]["beacon_uri"], None, beacon, methods=["GET", "POST"])
+    app.add_url_rule(data["implant"]["update_uri"], None, beacon, methods=["GET", "POST"])
+    app.run("0.0.0.0", 80)
