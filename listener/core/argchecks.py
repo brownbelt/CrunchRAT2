c5c63d8c0b6922d186790b5b7e86b3ce0149e088
root <t3ntman@gmail.com>
2017-04-16 20:45:36 -0600

    Temporarily removing profile checks

--- listener/core/argchecks.py
+++ listener/core/argchecks.py
@@ -6,45 +6,31 @@ from colorama import Fore, Style
 
 
 class ArgChecks(object):
-    def __init__(self):
-        self.protocol = None
-        self.external_address = None
-        self.port = None
-        self.profile = None
+    def __init__(self, protocol, external_address, port):
+        self.protocol = protocol
+        self.external_address = external_address
+        self.port = port
 
-    def protocol_check(self, protocol):
-        if protocol != "http" and protocol != "https":
+    def protocol_check(self):
+        if self.protocol != "http" and self.protocol != "https":
             print(Style.BRIGHT + Fore.RED + '[!] Invalid protocol supplied. Please enter "http" or "https".' + Style.RESET_ALL)
             sys.exit()
 
-        else:
-            self.protocol = protocol
-
-    def external_address_check(self, external_address):
+    def external_address_check(self):
         try:
-            ipaddress.ip_address(external_address)
-            self.external_address = external_address
+            ipaddress.ip_address(self.external_address)
 
         except:
             print(Style.BRIGHT + Fore.RED + "[!] Invalid external address supplied." + Style.RESET_ALL)
             sys.exit()
 
-    def port_check(self, port):
+    def port_check(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            s.connect(("localhost", port))
+            s.connect(("localhost", self.port))
             print(Style.BRIGHT + Fore.RED + "[!] Port already in use. Please choose another port." + Style.RESET_ALL)
             sys.exit()
 
         except:
-            self.port = port
-
-    def profile_check(self, profile):
-        try:
-            with open(profile) as f:
-                self.profile = json.loads(f.read())
-
-        except:
-            print(Style.BRIGHT + Fore.RED + "[!] Error parsing the provided JSON profile." + Style.RESET_ALL)
-            sys.exit()
+            pass
