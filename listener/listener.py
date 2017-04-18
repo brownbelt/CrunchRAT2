821cdfe872bad38ec380cf51b89031fe2fd0aa77
Hunter Hardman <t3ntman@gmail.com>
2017-04-18 17:29:32 -0600

    Re-organized and cleaned up code
    Thanks to Jake Valletta for his advice on Python programming.

--- listener/listener.py
+++ listener/listener.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 
 import argparse
-import json
 import os
-import pymysql
 import sys
-from colorama import Fore, Style
 from core.argchecks import ArgChecks
+from core.message import Message
 from core.webserver import WebServer
-from core.config import *
 
 
 if __name__ == "__main__":
     # if not ran with sudo or root privileges
     # exits the program
     if os.geteuid() != 0:
-        print(Style.BRIGHT + Fore.RED + "[!] Please re-run with sudo or root privileges." + Style.RESET_ALL)
+        Message.display_error("[!] Please re-run with sudo or root privileges.")
         sys.exit()
 
-    # parses arguments
     parser = argparse.ArgumentParser(prog="listener.py", description="CrunchRAT2 Listener - Written by Hunter Hardman @t3ntman")
     parser.add_argument("protocol", action="store", type=str, help="listener protocol [http][https]")
     parser.add_argument("external_address", action="store", type=str, help="listener external address")
     parser.add_argument("port", action="store", type=int, help="listener port")
     parser.add_argument("profile", action="store", type=str, help="listener profile")
+
+    # parses command-line arguments
     args = parser.parse_args()
 
-    # checks provided arguments for errors
-    a = ArgChecks(args.protocol, args.external_address, args.port, args.profile)
-    a.protocol_check()
-    a.external_address_check()    
-    a.port_check()
-    a.profile_check()
+    # TO DO: add in command-line argument checks here
 
-    # starts flask
-    w = WebServer(args.protocol, args.external_address, args.port, args.profile)
-    w.start_web_server()
+    # tries to instantiate WebServer class
+    try:
+        w = WebServer(args.protocol, args.external_address, args.port, args.profile)
+
+    # exception raised during WebServer class instantiation
+    # exits the program
+    except Exception as e:
+        Message.display_error("[!] Unable to establish database connection.\n" + str(e))
+        sys.exit()
