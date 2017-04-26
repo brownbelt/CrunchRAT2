f8175756ee1dc61566ab22b0bffbc8f02efb9a90
root <t3ntman@gmail.com>
2017-04-26 13:09:23 -0600

    Removing get_tasking() function temporarily

--- listener/core/tasking.py
+++ listener/core/tasking.py
@@ -23,7 +23,7 @@ class Tasking(object):
         DESCRIPTION:
             This function closes the database connection
 
-        RETURNS
+        RETURNS:
             None
         """
         if self.connection.open:
@@ -50,42 +50,6 @@ class Tasking(object):
                 # else no tasks
                 else:
                     return False
-
-        # exception raised during database query
-        except Exception:
-            raise
-
-    def generate_command_code(self, command):
-        """
-        DESCRIPTION:
-            This function generates Python 2.x code to execute a shell command
-
-        RETURNS:
-            String
-        """
-        # TO DO: add in full Popen() code here
-        # TO DO: query "listeners" table and add in update code
-        code = "import subprocess; subprocess.Popen('" + command + "', stdout=subprocess.PIPE, stderr=subprocess.PIPE)"
-        return code
-
-    def get_tasking(self, hostname, process_id):
-        """
-        DESCRIPTION:
-            This function gets tasking from the "tasks" table
-
-        RETURNS:
-            **** FILL OUT ****
-        """
-        # tries to query "tasks" table for tasking
-        # only one row is returned (if applicable)
-        try:
-            with self.connection.cursor() as cursor:
-                statement = "SELECT task_action, task_secondary FROM tasks WHERE hostname = %s AND process_id = %s LIMIT 1"
-                cursor.execute(statement, (hostname, process_id))
-                result = cursor.fetchone()
-
-                if result[0] == "command":
-                    return self.generate_command_code(result[1])
 
         # exception raised during database query
         except Exception:
