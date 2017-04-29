8eae03566707693108b278666d8dc04b7d268c49
root <t3ntman@gmail.com>
2017-04-28 23:26:15 -0600

    Added in 404 route

--- listener/core/webserver_new.py
+++ listener/core/webserver_new.py
@@ -8,35 +8,46 @@ from logging.handlers import RotatingFileHandler
 app = Flask(__name__)
 app.debug = True
 
-
-
 @app.route("/<path:path>")
 def catch_all(path):
+    """
+    DESCRIPTION:
+        This function redirects all other requests to "redirect_url"
+    """
+    return redirect(app.config["redirect_url"])
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    DESCRIPTION:
+        This function redirects all 404 requests to "redirect_url"
+    """
     return redirect(app.config["redirect_url"])
 
 
-def beacon_response():
-    return "beacon response"
 
+class WebServer(object):
+    def __init__(self, args):
+        self.protocol = args.protocol
+        self.external_address = args.external_address
+        self.port = args.port
+        self.profile = args.profile
 
+    def beacon_response(self):
+        return "beacon response"
 
-def start_flask(protocol, port, profile):
-    app.config["protocol"] = protocol
-    app.config["port"] = port
-    app.config["profile"] = profile
+    def start_flask(self):
+        app.config["protocol"] = self.protocol
+        app.config["port"] = self.port
+        app.config["profile"] = self.profile
 
-    # reads profile as a file
-    with open(profile) as file:
-        j = json.load(file)
+        # reads profile as a file
+        with open(self.profile) as file:
+            j = json.load(file)
 
-    app.config["redirect_url"] = j["implant"]["redirect_url"]
+        app.config["redirect_url"] = j["implant"]["redirect_url"]
 
-    app.add_url_rule("/beacon", None, beacon_response, methods=["GET", "POST"])
+        app.add_url_rule("/beacon", None, self.beacon_response, methods=["GET", "POST"])
 
-
-    log_handler = RotatingFileHandler("logs/access.log", maxBytes=100000000, backupCount=3)
-    app.logger.addHandler(log_handler)
-    app.logger.setLevel(logging.INFO)
-
-    server = WSGIServer(("0.0.0.0", port), app, log=app.logger)
-    server.serve_forever()
+        server = WSGIServer(("0.0.0.0", self.port), app, log=app.logger)
+        server.serve_forever()
