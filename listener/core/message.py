9e2644bbfb912eef0a9391a77463294945f1c229
root <t3ntman@gmail.com>
2017-04-29 10:06:02 -0600

    Added in comments

--- listener/core/message.py
+++ listener/core/message.py
@@ -2,17 +2,19 @@ from colorama import Fore, Style
 
 
 class Message(object):
-    
+
     @staticmethod
     def display_error(message):
-        '''
-            Description: Displays an error message with bold red coloring
-        '''
+        """
+        DESCRIPTION:
+            This function displays an error message with bold red coloring
+        """
         print(Style.BRIGHT + Fore.RED + message + Style.RESET_ALL)
 
     @staticmethod
     def display_status(message):
-        '''
-            Description: Displays a status message with bold green coloring
-        '''
+        """
+        DESCRIPTION:
+            This function displays a status message with bold green coloring
+        """
         print(Style.BRIGHT + Fore.GREEN + message + Style.RESET_ALL)
