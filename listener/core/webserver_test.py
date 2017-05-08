fa9a2f62326578c09fd2c4ee3120425b5491fdd7
t3ntman <t3ntman@gmail.com>
2017-05-08 08:51:33 -0600

    Re-added more functionality

--- listener/core/webserver_test.py
+++ listener/core/webserver_test.py
@@ -20,7 +20,7 @@ app.debug = True
 def page_not_found_redirect(error):
     """
     DESCRIPTION:
-        This function redirects all HTTP 404 requests to "redirect_url"
+        This function redirects all HTTP 404 requests to "redirect_url" (defined in profile)
     """
     return redirect(app.config["redirect_url"])
 
@@ -29,9 +29,16 @@ def page_not_found_redirect(error):
 def catch_all_redirect(path):
     """
     DESCRIPTION:
-        This function redirects all other HTTP requests to "redirect_url"
+        This function redirects all other HTTP requests to "redirect_url" (defined in profile)
     """
     return redirect(app.config["redirect_url"])
+
+
+def beacon_response():
+    """
+        This function is called when an implant beacons
+    """
+    return "beacon response"
 
 
 def start_flask_server(protocol, port, profile):
@@ -41,6 +48,18 @@ def start_flask_server(protocol, port, profile):
     """
     # tries to start the Flask server
     try:
+        # reads profile as a file
+        with open(profile) as file:
+            j = json.load(file)
+
+        # creates Flask global variable for "redirect_url"
+        app.config["redirect_url"] = j["implant"]["redirect_url"]
+
+        # adds beacon route
+        app.add_url_rule(j["implant"]["beacon_uri"], None, beacon_response, methods=["GET", "POST"])
+
+        # TO DO: add update route here
+
         # configures Flask logging with 100 meg max file size
         # all requests are logged to "listener/logs/access.log"
         log_handler = RotatingFileHandler("logs/access.log", maxBytes=100000000, backupCount=3)
