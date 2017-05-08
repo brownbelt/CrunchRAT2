791b7df43635ac2cb511a8bbda0645774597b308
t3ntman <t3ntman@gmail.com>
2017-05-08 08:27:45 -0600

    No need for a class here

--- listener/core/message.py
+++ listener/core/message.py
@@ -1,20 +1,17 @@
 from colorama import Fore, Style
 
 
-class Message(object):
+def print_error(message):
+    """
+    DESCRIPTION:
+        This function displays an error message with bold red coloring
+    """
+    print(Style.BRIGHT + Fore.RED + message + Style.RESET_ALL)
 
-    @staticmethod
-    def display_error(message):
-        """
-        DESCRIPTION:
-            This function displays an error message with bold red coloring
-        """
-        print(Style.BRIGHT + Fore.RED + message + Style.RESET_ALL)
 
-    @staticmethod
-    def display_status(message):
-        """
-        DESCRIPTION:
-            This function displays a status message with bold green coloring
-        """
-        print(Style.BRIGHT + Fore.GREEN + message + Style.RESET_ALL)
+def print_status(message):
+    """
+    DESCRIPTION:
+        This function displays a status message with bold green coloring
+    """
+    print(Style.BRIGHT + Fore.GREEN + message + Style.RESET_ALL)
