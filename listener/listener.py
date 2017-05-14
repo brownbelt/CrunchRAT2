e63c2c913a2c44d5c5c8965fa8ed666a934b02f5
t3ntman <t3ntman@gmail.com>
2017-05-14 11:17:08 -0600

    Added in PEP8 compliance

--- listener/listener.py
+++ listener/listener.py
@@ -1,13 +1,43 @@
 import argparse
-from core.webserver import *
+from core.webserver import WebServer
+from core.message import *
 
-parser = argparse.ArgumentParser(prog="listener.py", description="CrunchRAT2 Listener - Written by Hunter Hardman @t3ntman")
-parser.add_argument("protocol", action="store", type=str, help="listener protocol [http][https]")
-parser.add_argument("external_address", action="store", type=str, help="listener external address")
-parser.add_argument("port", action="store", type=int, help="listener port")
-parser.add_argument("profile", action="store", type=str, help="listener profile")
+# TO DO: add argument checks here
+
+parser = argparse.ArgumentParser(prog="listener.py",
+                                 description="CrunchRAT2 Listener - " +
+                                 "Written by Hunter Hardman @t3ntman")
+parser.add_argument("protocol",
+                    action="store",
+                    type=str,
+                    help="listener protocol [http][https]")
+parser.add_argument("external_address",
+                    action="store",
+                    type=str,
+                    help="listener external address")
+parser.add_argument("port",
+                    action="store",
+                    type=int,
+                    help="listener port")
+parser.add_argument("profile",
+                    action="store",
+                    type=str,
+                    help="listener profile")
 
 # parses command-line arguments
 args = parser.parse_args()
 
-start_flask_server(args.protocol, args.port, args.profile)
+# tries to start Flask listener
+try:
+    WebServer().start_flask_server(args.protocol,
+                                   args.external_address,
+                                   args.port,
+                                   args.profile)
+
+# this exception is raised if "listener.py"
+# is not ran with sudo or root privileges
+except IOError:
+    print_error("[!] Please re-run with sudo or root privileges.")
+
+except Exception as e:
+    print_error("[!] Error starting Flask web server:\n" + str(e))
