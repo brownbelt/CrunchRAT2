7419e34cc2ff1595757002b2644626f32379117b
t3ntman <t3ntman@gmail.com>
2017-05-14 15:51:44 -0600

    Added in "tasks" table to setup

--- listener/setup/database_setup.sql
+++ listener/setup/database_setup.sql
@@ -1,2 +1,3 @@
 CREATE TABLE listeners (protocol TEXT, external_address TEXT, port TEXT, profile TEXT);
 CREATE TABLE implants (hostname TEXT, operating_system TEXT, process_id TEXT, current_user TEXT, encryption_key TEXT, last_beacon TEXT);
+CREATE TABLE tasks (hostname TEXT, process_id TEXT, task_action TEXT, task_secondary TEXT, task_id TEXT);
