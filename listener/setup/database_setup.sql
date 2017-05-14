0bcb2edaddaf5a7b067b56790cb88633723e2e1f
t3ntman <t3ntman@gmail.com>
2017-05-14 12:19:34 -0600

    Added in "CREATE TABLE implants" statement

--- listener/setup/database_setup.sql
+++ listener/setup/database_setup.sql
@@ -1,1 +1,2 @@
 CREATE TABLE listeners (protocol TEXT, external_address TEXT, port TEXT, profile TEXT);
+CREATE TABLE implants (hostname TEXT, operating_system TEXT, process_id TEXT, current_user TEXT, encryption_key TEXT, last_beacon TEXT);
