b712af60cc44397705affd703944e25591e6e754
t3ntman <t3ntman@gmail.com>
2017-05-21 15:41:55 -0600

    Added very rough authentication code

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ from flask import session
 from gevent.wsgi import WSGIServer
 import os
 
-
 # creates Flask application instance
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
@@ -31,34 +30,44 @@ def internal_server_error(e):
     return render_template("500.html"), 500
 
 
+
+
+
 # Login page
-@app.route("/login")
+@app.route("/login", methods=["GET"])
 def login():
+    session["logged_in"] = False
     return render_template("login.html"), 200
+
+
+def check_authentication(password):
+    if password == args.password:
+        return True
+
+    else:
+        return False
 
 
 # Login form submit function
 @app.route("/loginSubmit", methods=["POST"])
 def login_submit():
     password = request.form["password"]
-
-    if password == args.password:
-        flash("success")
-        return "successful authentication"
-
-    else:
-        flash("failure")
-        return "failed authentication"
+    print(check_authentication(password))
+    return ""
 
 
-# this is the index "view"
-@app.route("/")
+
+
+
+
+# redirects all HTTP GET / requests to /login
+@app.route("/", methods=["GET"])
 def index():
     return redirect("/login", 302)
 
 
 # this is the user "view"
-@app.route("/user/<string:name>")
+@app.route("/user/<string:name>", methods=["GET"])
 def user(name):
     return "<h1>hello, %s!</h1>" % name
 
