a786ccc343aa4fd4e3e2a173d18523b58a63b20e
t3ntman <t3ntman@gmail.com>
2017-05-21 07:32:29 -0600

    Added in HTTP 403, 404, and 500 error handlers

--- server.py
+++ server.py
@@ -2,14 +2,26 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-
 # creates Flask application instance
 app = Flask(__name__)
 
 
+# HTTP 403
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template("403.html"), 403
+
+
+# HTTP 404
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+
+# HTTP 500
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 
 # this is the index "view"
