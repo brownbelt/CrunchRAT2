0189982121180bcca50e9dd8846626b848ad9e91
t3ntman <t3ntman@gmail.com>
2017-05-23 06:56:45 -0600

    Added working authentication code

---
+++ app2.py
@@ -0,0 +1,85 @@
+# Working authentication code
+# Source: https://github.com/maxcountryman/flask-login
+
+import flask
+import flask_login
+from gevent.wsgi import WSGIServer
+import os
+
+app = flask.Flask(__name__)
+app.secret_key = 'super secret string'  # Change this!
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+# Our mock database.
+users = {'foo@bar.tld': {'pw': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['pw'] == users[email]['pw']
+
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'></input>
+                <input type='password' name='pw' id='pw' placeholder='password'></input>
+                <input type='submit' name='submit'></input>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if flask.request.form['pw'] == users[email]['pw']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+if __name__ == "__main__":
+    # starts Flask listener
+    server = WSGIServer(("0.0.0.0", 80), app)
+    server.serve_forever()
