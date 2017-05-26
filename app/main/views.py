ac9c93ca48a1a3f546cc335b6235dfca4538ddda
Hunter Hardman <t3ntman@gmail.com>
2017-05-26 11:17:15 -0600

    Fixed typo

--- app/main/views.py
+++ app/main/views.py
@@ -24,4 +24,4 @@ def page_not_found(e):
 # HTTP 500 route
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template("404.html"), 404
+    return render_template("500.html"), 500
