5983d026fdfbd3eb02cacdbda88232177571e9ef
t3ntman <t3ntman@gmail.com>
2017-05-26 13:53:06 -0600

    Restructuring Flask code again

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,3 @@
-from app.main.views import main
-from app.admin.views import admin
 from flask import Flask
 
 app = Flask(__name__)
-
-app.register_blueprint(main, url_prefix="/")
-app.register_blueprint(admin, url_prefix="/admin")
