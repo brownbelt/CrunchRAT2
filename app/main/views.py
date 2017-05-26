df85ff2e3d07ea31b7aa14b27d37a10b02bc1313
t3ntman <t3ntman@gmail.com>
2017-05-26 14:01:39 -0600

    Trying to fix an app import bug

--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,7 @@
-from flask import Blueprint
 from flask import render_template
-
-main = Blueprint("main", __name__)
-
-
-@main.route("/")
-def index():
-    return "main"
+#from . import app
 
 
-# HTTP 403 route
-@main.app_errorhandler(403)
-def forbidden(e):
-    return render_template("403.html"), 403
-
-
-# HTTP 404 route
-@main.app_errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
-# HTTP 500 route
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html"), 500
+@app.route("/test")
+def test():
+    return "this is a test page"
