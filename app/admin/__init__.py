3e2896e5ea5678cb07420548b3c2f89ec25cfae6
t3ntman <t3ntman@gmail.com>
2017-05-26 11:05:54 -0600

    More restructuring of Flask

---
+++ app/admin/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from . import views
+
+auth = Blueprint("admin", __name__)
---
+++ app/main/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from . import views
+
+auth = Blueprint("main", __name__)
