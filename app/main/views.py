ddee4d73df5a394e786859b556ca6647293a1097
Hunter Hardman <t3ntman@gmail.com>
2017-05-26 11:15:51 -0600

    Added in HTTP error views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,27 @@
 from flask import Blueprint
+from flask import render_template
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/")
 def index():
     return "main"
+
+
+# HTTP 403 route
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template("403.html"), 403
+
+
+# HTTP 404 route
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# HTTP 500 route
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template("404.html"), 404
