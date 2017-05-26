16bd2fa027c32b0b2ef071fa71beef0977a0c14c
t3ntman <t3ntman@gmail.com>
2017-05-26 10:40:25 -0600

    Added in gevent WSGI

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+from gevent.wsgi import WSGIServer
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    # starts Flask server
+    server = WSGIServer(("0.0.0.0", 80), app)
+    server.serve_forever()
