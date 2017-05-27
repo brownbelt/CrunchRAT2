6dcb2afaa588dbaf3446c5693fb4ac0c8637b168
t3ntman <t3ntman@gmail.com>
2017-05-26 20:14:00 -0600

    Removed HTTP errors from this file

--- app/views.py
+++ app/views.py
@@ -10,8 +10,3 @@ def index():
 @app.route("/login", methods=["GET"])
 def login():
     return "login page here"
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
