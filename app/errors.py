a1c31fa8a5c4760255b8a0d7b22cde34b595599f
t3ntman <t3ntman@gmail.com>
2017-05-26 20:19:36 -0600

    Added in HTTP 405 error

--- app/errors.py
+++ app/errors.py
@@ -14,6 +14,12 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
+# HTTP 405
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template("405.html"), 405
+
+
 # HTTP 500
 @app.errorhandler(500)
 def internal_server_error(e):
