4ea69e05b7a3305631f09bc732e1651f355ebbb2
t3ntman <t3ntman@gmail.com>
2017-05-29 14:13:01 -0600

    Added in debugging authentication code

--- app/views.py
+++ app/views.py
@@ -1,17 +1,84 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
+from flask_login import login_required, LoginManager, UserMixin
 from app import app
 
+# AUTHENTICATION DEBUGGING!
+# Source: https://github.com/maxcountryman/flask-login
 
-@app.route("/", methods=["GET"])
-def index():
-    return redirect("/login", code=302)
+import flask
+import flask_login
 
 
-@app.route("/login", methods=["GET"])
+class User(UserMixin):
+    pass
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template("401.html"), 401
+
+
+
+users = {'admin': {'pw': 'secret'}}
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['pw'] == users[email]['pw']
+
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html"), 200
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'></input>
+                <input type='password' name='pw' id='pw' placeholder='password'></input>
+                <input type='submit' name='submit'></input>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if flask.request.form['pw'] == users[email]['pw']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
 
 
-@app.route("/loginSubmit", methods=["POST"])
-def login_submit():
-    return "you submitted the login page"
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
