7c0b225290bb5cf8c3d96edc4df97d8b6dfeead5
t3ntman <t3ntman@gmail.com>
2017-06-12 09:22:54 -0600

    Added in global variable

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ from colorama import Fore, Style
 from gevent.wsgi import WSGIServer
 from app import app
 
+# global variable for server password
+# TO DO: see if there's an alternative to this
+global server_password
+
+
 if __name__ == '__main__':
     # command-line argument parsing
     parser = argparse.ArgumentParser(prog='server.py',
@@ -15,13 +20,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # sets server password
+    server_password = args.password
+
     # tries to start Flask server on tcp/8888
     try:
         server = WSGIServer(('0.0.0.0', 8888), app)
         print(Fore.GREEN + '[+] Server started on tcp/8888.' + Style.RESET_ALL)
         server.serve_forever()
 
-    # KeyboardInterrupt Exception
+    # KeyboardInterrupt exception
     except KeyboardInterrupt:
         print(Fore.GREEN + '[+] Server stopped on tcp/8888.' + Style.RESET_ALL)
 
